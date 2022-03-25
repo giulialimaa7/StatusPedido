0646df7dc29547130d8bc32d1e3c36a8201fcd13
giulia lima <giulia.arocontabil@gmail.com>
2022-03-25 09:36:24 -0300

    Status Pedido

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleApp12.Entidades;
+using ConsoleApp12.Entidades.Enums;
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PedidoOrder pedido = new PedidoOrder
+            {
+                Id = 1080,
+                Momento = DateTime.Now,
+                Status = OrderStatus.PagamentoPendente
+            };
+            Console.WriteLine(pedido);
+
+            string txt = OrderStatus.PagamentoPendente.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Entregue");
+            Console.WriteLine(os);
+        }
+    }
+}
